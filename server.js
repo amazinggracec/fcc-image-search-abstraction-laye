9a213f0552cd9ee945f6d1e7f1094504f7c1f619
amazinggracec <grace_gc@hotmail.com>
2016-02-02 17:16:25 +0000

    testing

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var port = process.env.PORT;
 var MongoClient = require('mongodb').MongoClient;
 var mongo_url = process.env.MONGO_URI;
 
-var searches = require('./search.js');
+/*var searches = require('./search.js');
 var home = require('./index.js');
 
 searches(MongoClient, mongo_url, app);
 home(MongoClient, mongo_url, app);
-
+*/
 //to start server
 app.listen(port || 8080, process.env.IP || "0.0.0.0", function(){
     console.log("server listening at", port);
